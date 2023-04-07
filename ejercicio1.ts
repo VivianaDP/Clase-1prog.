abbc279583fa9f26a119c45e6d85311e4ff06c90
Viviana <vishena676@gmail.com>
2023-04-07 16:50:54 -0300

    ejerc.1.fin

---
+++ ejercicio1.ts
@@ -0,0 +1,11 @@
+
+// let mensaje: string = "Soy la mas mejor";
+// console.log(mensaje);
+
+function practica(nombre:string): string {
+return "Hola que tal soy..." + nombre
+}
+let nombre: string;
+nombre = 'Maximiliano';
+
+console.log(practica(nombre));
